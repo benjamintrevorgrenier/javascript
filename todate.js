53ea10da90fceed949a6af5c65cfb67693e042bc
Benjamin Trevor Grenier <61502669+benjamintrevorgrenier@users.noreply.github.com>
2021-05-23 15:49:52 +1000

    Create todate.js

---
+++ todate.js
@@ -0,0 +1,19 @@
+var lives=3;
+
+var example= "I am a variable";
+
+document.write(example);
+
+example = "This is an example for the same variable";
+
+document.write(example);
+
+document.write(lives);
+
+console.log("This is text that will go directly to developers");
+
+var VisitorAge = prompt("How old are you");
+document.write(VisitorAge);
+
+alert("You are busted");
+document.write("HELLOOOOO");
