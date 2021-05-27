a41c474467ceaf8aea89201a97e91bc004c9f543
Benjamin Trevor Grenier <61502669+benjamintrevorgrenier@users.noreply.github.com>
2021-05-27 14:53:22 +1000

    Create daysalivecalculator.js

---
+++ daysalivecalculator.js
@@ -0,0 +1,4 @@
+var VisitorAge = prompt("How old are you? ");
+var YearsAlive = parseInt(VisitorAge);
+YearsAlive = YearsAlive * 365;
+document.write("You have been alive for " + YearsAlive +"days!!!");
