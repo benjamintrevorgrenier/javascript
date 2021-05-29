fcf717cd841500b80dfc3df8c15826553d6d6e9e
Benjamin Trevor Grenier <61502669+benjamintrevorgrenier@users.noreply.github.com>
2021-05-29 14:32:54 +1000

    Create inputerng.js

---
+++ inputerng.js
@@ -0,0 +1,4 @@
+var UserNumber = prompt("Enter your first number: ");
+ UserNumber = parseInt(UserNumber);
+ var RNumber = Math.floor(Math.random() * UserNumber) +1;
+ alert("Your Randiom Number is " + RNumber);
